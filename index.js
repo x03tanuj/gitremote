557bfb9a4964fee95a7fba17d22ab8b841839bb8
Tanuj <tanujx03@outlook.com>
2026-03-08 17:04:24 +0530

    Commit after merging

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+
+// This is a simple JavaScript file that prints "Hello World" to the console. It serves as a basic example of how to use console.log() to output text in JavaScript.
+
+// This is a comment
+// This is another comment
+/* This is a multi-line comment
+   that spans multiple lines
+   and is useful for longer explanations or documentation. */
