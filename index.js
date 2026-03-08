1a450fdee1177a7209309f4678fc7aaabae7a0ec
Tanuj <tanujx03@outlook.com>
2026-03-08 11:32:22 +0530

    Commit from login

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("Hello World");
+
+
+// This is a simple JavaScript file that prints "Hello World" to the console. It serves as a basic example of how to use console.log() to output text in JavaScript.
