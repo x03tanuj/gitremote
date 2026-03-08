6c2d27db95c4ec2930d48d380de7bc7620b1bead
Tanuj <tanujx03@outlook.com>
2026-03-08 11:34:45 +0530

    from main

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+
+// This is a comment
+// This is another comment
+/* This is a multi-line comment
+   that spans multiple lines
+   and is useful for longer explanations or documentation. */
